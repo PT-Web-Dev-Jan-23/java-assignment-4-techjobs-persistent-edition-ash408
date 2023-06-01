953b596fcf8121d7eb9dfb51c492f33afe5d5658
JaneDoe <janedoe@notanemail.com>
2023-06-01 18:55:43 -0500

    Finished assignment

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 -- Part 1: Test it with SQL
-
+SELECT column_name, data_type FROM information_schema.columns WHERE table_name = "job"
 -- Part 2: Test it with SQL
 SELECT name FROM employer WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
